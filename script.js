3d74816948c42c44653df6c0c8b73b9ce45e570c
theusan777 <theusant777@icloud.com>
2026-02-26 20:49:22 -0300

    showing footer

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const GBP = 6.95
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
 
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g
@@ -29,5 +30,12 @@ form.onsubmit = (event) => {
 }
 
 function convertCurrency(amount, price, symbol){
-  console.log(amount, price, symbol)
-}
+  try {
+    footer.classList.add("show-result")
+  }
+ catch (error){
+  console.log(error)
+  footer.classList.remove("show-result")
+  alert("Não foi possível converter. Tente novamente mais tarde.")
+  }
+ }
