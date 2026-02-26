6b8381a61968528910863b737a9baef9cebc73d3
theusan777 <theusant777@icloud.com>
2026-02-25 21:17:53 -0300

    identifying the currency

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const USD = 5.12
+const EUR = 6.06
+const GBP = 6.95
+
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
@@ -10,5 +14,20 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
   event.preventDefault()
 
-  console.log(currency.value)
+  switch (currency.value){
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+      
+  }
+}
+
+function convertCurrency(amount, price, symbol){
+  console.log(amount, price, symbol)
 }
