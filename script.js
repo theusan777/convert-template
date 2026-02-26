4370e7996377cc0edd924f03c7a679cc49c69274
theusan777 <theusant777@icloud.com>
2026-02-25 21:00:20 -0300

    obtaining the selected currency

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  console.log(currency.value)
+}
